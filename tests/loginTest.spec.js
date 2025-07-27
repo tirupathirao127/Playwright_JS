0e3ff1ced82418765d5cd7451da648f463dd4887
Tirupathi Rao <tirupathirao127@gmail.com>
2025-07-27 10:26:42 +0530

    login with invalid credentials

--- tests/loginTest.spec.js
+++ tests/loginTest.spec.js
@@ -16,4 +16,18 @@ test.describe('Login Tests', () => {
         expect(page.isClosed()).toBeTruthy()
     })
 
+    test('login with invalid credentials', async ({page}) => {
+        await page.goto("https://www.saucedemo.com/v1/index.html")
+
+        await expect(page.locator("#user-name")).toBeVisible()
+        await page.locator("#user-name").fill("invalid_user")
+        await page.locator("#password").fill("invalid_password")
+        await page.locator("#login-button").click()
+
+        await expect(page.locator('[data-test="error"]')).toContainText("Username and password do not match any user in this service")
+
+        await page.close()
+        expect(page.isClosed()).toBeTruthy()
+    })
+
 })
